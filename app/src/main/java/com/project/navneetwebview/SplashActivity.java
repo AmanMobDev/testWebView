da861793c135b778fe54b767b9840be8f8401900
AmanMobDev <75611515+AmanMobDev@users.noreply.github.com>
2021-02-06 15:28:58 +0530

    Splash Activity added

---
+++ app/src/main/java/com/project/navneetwebview/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.project.navneetwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        ImageView image = findViewById(R.id.splash_logo);
+
+        Glide.with( getApplicationContext() ).load( R.drawable.wifi ).into( image );
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },SPLASH_TIME);
+
+
+
+
+    }
+}
